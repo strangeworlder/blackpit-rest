2c925efc5323a993052ce33373779527378be3a8
Petri Leinonen <petri.leinonen@striim.in>
2016-10-28 20:28:42 +0300

    even more actual db

--- app.js
+++ app.js
@@ -1,54 +1,6 @@
-// Check dependencies
-var http = require('http');
-// Create the http server.
-// reference: http://net.tutsplus.com/tutorials/javascript-ajax/node-js-for-beginners/
-
-/***************
-* Correction 1: Using the request.on('close', function()( ... )-listener isn't required anymore
-***************/
-http.createServer(function(req, res) {
-    console.log('Receving request...');
-    var callback = function(err, result) {
-        res.writeHead(200, {
-            'Content-Type' : 'x-application/json'
-        });
-        console.log('json:', result);
-        res.end(result);
-    };
-
-    getSQL(callback);
-
-}).listen(3000);
-
-// Access MySQL via node-mysql
-// https://github.com/felixge/node-mysql
-function getSQL(callback) {
-    var mysql = require('mysql');
-    var connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
 
 
-    connection.connect();
-    var json = '';
-    var query = 'SELECT * FROM test';
-    connection.query(query, function(err, results, fields) {
-        if (err)
-            return callback(err, null);
-
-        console.log('The query-result is: ', results[0]);
-
-        // wrap result-set as json
-        json = JSON.stringify(results);
-
-        /***************
-        * Correction 2: Nest the callback correctly!
-        ***************/
-        connection.end();
-        console.log('JSON-result:', json);
-        callback(null, json);
-    });
-};
-
-/*var express = require('express');
+var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
