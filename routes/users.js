4cd48c1c315ce1f7888ff6faa14ab61b22897c78
Petri Leinonen <petri.leinonen@striim.in>
2016-10-28 20:38:14 +0300

    even more actual db

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
 var connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
 
 
@@ -7,6 +9,8 @@ var connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
  * GET userlist.
  */
 router.get('/userlist', function(req, res) {
+  connection.connect();
+
   connection.query('SELECT name FROM test', function(err, rows, fields) {
     if (err) throw err;
               console.log('Connection result error '+err);
@@ -14,6 +18,7 @@ router.get('/userlist', function(req, res) {
                       response.writeHead(200, { 'Content-Type': 'application/json'});
               response.end(JSON.stringify(rows));
       });
+      connection.end();
 
 });
 
