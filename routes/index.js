b5056ec4c6445455872ec31ec8d0182077b0bcb1
Petri Leinonen <petri.leinonen@striim.in>
2016-10-29 12:05:50 +0300

    moar testing

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  connection.connect();
 
-  connection.query('SELECT name FROM test', function(err, rows, fields) {
-    if (err) throw err;
-
-    res.render('index', { title: rows[0].name });
+    res.render('index', { title: 'bloop' });
   });
 
-  connection.end();
 
 
 });
