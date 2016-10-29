96cf53d0bedfb3d0180c88c5f501fe2980ab088e
Petri Leinonen <petri.leinonen@striim.in>
2016-10-29 16:48:23 +0300

    moar testing

--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,8 @@ router.get('/', function(req, res) {
       });
 });
 router.get('/2016/:kierrosId', function(req, res) {
-  var kierros = parseInt(req.param('kierrosId')) + 1;
+  var kierros = 0;
+  kierros = kierros + parseInt(req.param('kierrosId')) + 1;
 
   db.query('SELECT m.* FROM sarjataulukkodata m LEFT JOIN sarjataulukkodata b ON m.team_id = b.team_id AND m.kierros_id < b.kierros_id AND b.kierros_id < '+kierros+' AND b.season_id = 1 WHERE b.kierros_id IS NULL AND m.kierros_id < '+kierros+' m.season_id = 1 ORDER BY m.team_id', function(err, rows, fields) {
       console.log('Connection result error '+err);
