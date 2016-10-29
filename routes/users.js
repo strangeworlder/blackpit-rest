f762045476e853e1d7fb21e1569378985143fb25
Petri Leinonen <petri.leinonen@striim.in>
2016-10-29 11:11:39 +0300

    moar testing

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var db = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
 
-
-/*
- * GET userlist.
- */
-router.get('/', function(req, res) {
-  db.connect();
-
-  db.query('SELECT name FROM test', function(err, rows, fields) {
-    if (err) throw err;
-              console.log('Connection result error '+err);
-              console.log('no of records is '+rows.length);
-
-      });
-
-      db.end();
-
-
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
