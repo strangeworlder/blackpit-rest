76a39d655d72558d15fae5666c6d0ecbd8f4efd8
Petri Leinonen <petri.leinonen@striim.in>
2016-10-30 09:35:28 +0200

    moar

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ function stripAlphaChars(source) {
  */
 router.get('/', function(req, res) {
 
-  db.query('SELECT m.* FROM sarjataulukkodata m LEFT JOIN sarjataulukkodata b ON m.team_id = b.team_id AND m.kierros_id < b.kierros_id AND b.season_id = 1 WHERE b.kierros_id IS NULL AND m.season_id = 1 ORDER BY m.team_id', function(err, rows, fields) {
+  db.query('SELECT m.* FROM sarjataulukkodata m LEFT JOIN sarjataulukkodata b ON m.team_id = b.team_id AND m.kierros_id < b.kierros_id AND b.season_id = 1 WHERE b.kierros_id IS NULL AND m.season_id = 1 ORDER BY (m.voitot*3+m.tasan) DESC, (m.tdf-m.tda) DESC, (m.casf-m.casa) DESC', function(err, rows, fields) {
       console.log('Connection result error '+err);
       if (err) throw err;
 
@@ -27,7 +27,7 @@ router.get('/', function(req, res) {
 router.get('/2016/:kierrosId', function(req, res) {
   var kierros = 0;
   kierros = kierros + parseInt(req.params.kierrosId) + 1;
-  var sql = 'SELECT m.* FROM sarjataulukkodata m LEFT JOIN sarjataulukkodata b ON m.team_id = b.team_id AND m.kierros_id < b.kierros_id AND b.kierros_id < '+db.escape(kierros)+' AND b.season_id = 1 WHERE b.kierros_id IS NULL AND m.kierros_id < '+db.escape(kierros)+' AND m.season_id = 1 ORDER BY m.team_id';
+  var sql = 'SELECT m.* FROM sarjataulukkodata m LEFT JOIN sarjataulukkodata b ON m.team_id = b.team_id AND m.kierros_id < b.kierros_id AND b.kierros_id < '+db.escape(kierros)+' AND b.season_id = 1 WHERE b.kierros_id IS NULL AND m.kierros_id < '+db.escape(kierros)+' AND m.season_id = 1 ORDER BY (m.voitot*3+m.tasan) DESC, (m.tdf-m.tda) DESC, (m.casf-m.casa) DESC';
   db.query(sql, function(err, rows, fields) {
       console.log('Connection result error '+err);
       if (err) throw err;
