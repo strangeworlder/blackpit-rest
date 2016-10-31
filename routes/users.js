ab8ffe667671d55180d2d8e8919281bdd3475ebd
Petri Leinonen <petri.leinonen@striim.in>
2016-10-31 06:52:47 +0200

    wiiild

--- routes/users.js
+++ routes/users.js
@@ -14,10 +14,21 @@ function stripAlphaChars(source) {
  */
 router.get('/', function(req, res) {
 
-  db.query('SELECT m.* FROM sarjataulukkodata m LEFT JOIN sarjataulukkodata b ON m.team_id = b.team_id AND m.kierros_id < b.kierros_id AND b.season_id = 1 WHERE b.kierros_id IS NULL AND m.season_id = 1 ORDER BY (m.voitot*3+m.tasan) DESC, (m.tdf-m.tda) DESC, (m.casf-m.casa) DESC', function(err, rows, fields) {
+  var sql = 'SELECT m.* FROM sarjataulukkodata m '+
+              'LEFT JOIN joukkuedata j '+
+              'ON m.team_id = j.team_id AND m.kierros_id = j.kierros_id AND j.season_id = m.season_id AND j.series_id = m.series_id '+
+              'LEFT JOIN sarjataulukkodata b '+
+              'ON m.team_id = b.team_id AND m.kierros_id < b.kierros_id AND b.season_id = m.season_id AND b.series_id = m.series_id '+
+              'WHERE b.kierros_id IS NULL '+
+              'AND m.season_id = 1 '+
+              'ORDER BY (m.voitot*3+m.tasan) DESC, (m.tdf-m.tda) DESC, (m.casf-m.casa) DESC';
+  db.query(sql, function(err, rows, fields) {
       console.log('Connection result error '+err);
       if (err) throw err;
-
+      var i = 1;
+      rows.forEach(function(element) {
+          element['ranking'] = i++;
+      });
       console.log('Connection result error '+err);
       console.log('no of records is '+rows.length);
       res.json(rows);
